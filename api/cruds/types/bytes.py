e3f6bd51fc2339354c8f3301e39d2fbb52409afb
Colk-tech <master@colk.dev>
2022-12-27 01:02:49 +0900

    [追加] SQLAlchemy で使える Bytes 型を定義

---
+++ api/cruds/types/bytes.py
@@ -0,0 +1,15 @@
+from sqlalchemy.types import String, TypeDecorator
+
+
+class HexByteString(TypeDecorator):
+    """Convert Python bytestring to string with hexadecimal digits and back for storage."""
+
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        if not isinstance(value, bytes):
+            raise TypeError("HexByteString columns support only bytes values.")
+        return value.hex()
+
+    def process_result_value(self, value, dialect):
+        return bytes.fromhex(value) if value else None
